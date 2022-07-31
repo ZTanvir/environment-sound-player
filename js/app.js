b01c3e5dd7af180aee9cced94fccabbaee5bc10f
Zahirul Islam Tanvir <zahirulopel@gmail.com>
2022-07-31 20:28:44 +0600

    Project Animal Sound Player done

--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
-window.addEventListener("keydown", function (e) {
+// When btn clicked the corrosponding audio file will play
+// Add a border the button the user has pressed
+function play(e) {
     const audio = document.querySelector(`audio[data-key="${e.keyCode}`);
     const keyPress = document.querySelector(`.key[data-key="${e.keyCode}`);
-    console.log(keyPress);
     if (audio == null) return;
     audio.currentTime = 0;
     audio.play();
     keyPress.classList.add("playing");
-});
+}
+window.addEventListener("keydown", play);
+
+let allBtn = document.querySelectorAll(".key");
+allBtn = [...allBtn];
+
+// Remove the border after a little time of press
+function removeBorder(e) {
+    if (e.propertyName != "transform") {
+        return;
+    } else {
+        e.target.classList.remove("playing");
+    }
+}
+allBtn.forEach((btn) => btn.addEventListener("transitionend", removeBorder));
